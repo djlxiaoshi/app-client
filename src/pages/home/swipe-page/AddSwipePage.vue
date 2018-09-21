d009aedb7a2cd29a2b224d43e2952b19b672051f
1281233206@qq.com <1281233206@qq.com>
2018-09-21 21:08:53 +0800

    添加书签mock成功

--- src/pages/home/swipe-page/AddSwipePage.vue
+++ src/pages/home/swipe-page/AddSwipePage.vue
@@ -2,7 +2,14 @@
   <transition name="page-move">
     <cube-page title="添加" v-on="$listeners">
       <div slot="content">
-        辅导班国防部
+        <cube-form :model="model" @validate="validateHandler" @submit="submitHandler">
+          <cube-form-group>
+            <cube-form-item v-for="(field, index) in fields" :field="field" :key="index"></cube-form-item>
+          </cube-form-group>
+          <cube-form-group>
+            <cube-button type="submit">Submit</cube-button>
+          </cube-form-group>
+        </cube-form>
       </div>
     </cube-page>
   </transition>
@@ -10,17 +17,83 @@
 
 <script>
   import CubePage from '@/components/common/swipe-page/SwipePage';
-    export default {
-      name: 'swipe-page',
-      components: {
-        CubePage
-      },
-      methods: {
-        back () {
 
+  export default {
+    components: {
+      CubePage
+    },
+    data () {
+      return {
+        validity: {},
+        valid: undefined,
+        model: {
+          titleValue: '',
+          urlValue: '',
+          tagsValue: ''
+        },
+        fields: [
+          {
+            type: 'input',
+            modelKey: 'titleValue',
+            label: '标题',
+            props: {
+              placeholder: '请输入'
+            },
+            rules: {
+              required: true
+            }
+          },
+          {
+            type: 'input',
+            modelKey: 'urlValue',
+            label: 'url',
+            props: {
+              placeholder: '请输入URL'
+            },
+            rules: {
+              required: true
+            }
+          },
+          {
+            type: 'input',
+            modelKey: 'tagsValue',
+            label: '分类',
+            props: {
+              placeholder: '请输入'
+            }
+          }
+        ]
+      };
+    },
+    methods: {
+      submitHandler (e) {
+        if (this.valid) {
+          this.addLinkItem();
         }
+      },
+      addLinkItem () {
+        this.$http({
+          url: '/favorite/',
+          method: 'post',
+          data: {
+            title: this.model.titleValue,
+            url: this.model.urlValue,
+            source: '个人网站',
+            time: '2018-9-19',
+            tags: [this.model.tagsValue]
+          },
+          hasWarning: true
+        }).then(result => {
+
+        });
+      },
+      validateHandler (result) {
+        this.validity = result.validity;
+        this.valid = result.valid;
       }
-    };
+    }
+  };
+
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
